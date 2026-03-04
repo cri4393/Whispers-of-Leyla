a93c1839f49e06085f97de5a71b078323589766c
cri4393 <cristianseles10@gmail.com>
2026-03-04 12:27:34 +0100

    -

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -18,14 +18,14 @@ public class PlayerMovements : MonoBehaviour
     const string JUMP_LIMPS = "LimpsJump";
     const string IDLE = "Iris_Idle";
     const string RUN = "Iris_Run";
-    const string JUMP = "Jump";
-    const string JUMP_MOVING = "JumpMoving";
-    const string JUMP_MOVINGDOWN = "JumpMovingDown";
-    const string JUMP_FALLING = "Falling";
+    const string JUMP = "Iris_Jump";
+    const string JUMP_MOVING = "Iris_JumpMoving";
+    const string JUMP_MOVINGDOWN = "Iris_JumpMovingDown";
+    const string JUMP_FALLING = "Iris_Falling";
     const string FALLING_MAXSPEED = "FallMaxSpeed";
-    const string SLIDING = "WallSlide";
-    const string WALLJUMP = "WallJump";
-    const string DASH = "Dash";
+    const string SLIDING = "Iris_WallSliding";
+    const string WALLJUMP = "Iris_WallJump";
+    const string DASH = "Iris_Dash";
     #endregion
 
     #region Variables Movement
@@ -71,8 +71,24 @@ public class PlayerMovements : MonoBehaviour
     public Vector2 wallCheckSize;
     public LayerMask wallslideLayer;
     public bool isWallSliding;
-    public bool canSliding;
-    private bool canWallJumping = false;
+    public bool canSliding = true;
+    private bool canWallJumping = true;
+    #endregion
+
+    #region Variables Dash
+    [Header("DASH")]
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+    public float dashStunRadius;
+    [SerializeField] private LayerMask enemyLayer;
+    private bool canDash = true;
+    public bool haveStunDash = false;
+    [HideInInspector] public bool isDashing;
+    private bool isDashingCooldown = false;
+    public bool haveDash = true;
+    public GameObject dashParticleRight;
+    public GameObject dashParticleLeft;
     #endregion
 
     #region Timers
@@ -102,6 +118,7 @@ public class PlayerMovements : MonoBehaviour
     private BoxCollider2D box;
 
     public bool isLimps = false;
+    public bool canPressOnlyJump = false;
 
     void Awake()
     {
@@ -180,16 +197,16 @@ public class PlayerMovements : MonoBehaviour
         // }
 
         CheckWorld();
-        //ResetDash();
+        ResetDash();
         //LerpingY();
 
-        //Timers();
+        Timers();
         CheckForMaxFallSpeed();
         Gravity();
 
         CollisionsChecks();
-        //JumpChecks();
-        //SlideChecks();
+        JumpChecks();
+        SlideChecks();
 
         
         /*
@@ -201,11 +218,10 @@ public class PlayerMovements : MonoBehaviour
             }
         }
         */
+        
+        if (LastOnGroundTime > 0 || LastOnWallTime < 0)
+             IsAlreadySliding = false;
 
-        // if (LastOnGroundTime > 0 || LastOnWallTime < 0)
-        //     IsAlreadySliding = false;
-
-        /*
         if (IsAlreadySliding)
         {
             IsSliding = true;
@@ -213,8 +229,6 @@ public class PlayerMovements : MonoBehaviour
         }
         else
             SlideChecks();
-        */
-        
         // if(PauseManager.paused) return;
         // pad = Gamepad.current;
     }
@@ -236,10 +250,10 @@ public class PlayerMovements : MonoBehaviour
         //     return;
         // }
 
-        // if (isDashing)
-        // {
-        //     return;
-        // }
+        if (isDashing)
+        {
+             return;
+        }
         Flip();
 
         // if(NewPlayerCombat.instance.attackUp || NewPlayerCombat.instance.attackDown) return;
@@ -247,42 +261,115 @@ public class PlayerMovements : MonoBehaviour
         // if (isSit) return;
         if (isLimps)
             Inputs();
-        // else if (canPressOnlyJump)
-        //     RunControlledJump();
+        else if (canPressOnlyJump)
+            RunControlledJump();
         else if (!IsWallJumping /* && !playerLife.knockback  && !knockbackScript.IsBeingKnockedBack && !isLimps*/)
             Run(1);
 
-        //if (canPressOnlyJump) return;
-        //if (IsSliding)
-        //    Slide();
+        if (canPressOnlyJump) return;
+        if (IsSliding)
+            Slide();
 
     //     if (isLimps && LastOnGroundTime < 0)
     //         ChangeAnimationState(JUMP_LIMPS);
 
     //     if (isLimps || isSittingUp 
     //         || NewPlayerCombat.instance.isAttacking || NewPlayerCombat.instance.isAttackingH && NewDialogueManager.GetInstance().dialogueIsPlaying) return;
-    //     if(LastOnGroundTime > 0 && !isSleepAnim && !isAwakening)
-    //     {
+    if(LastOnGroundTime > 0 /*&& !isSleepAnim && !isAwakening*/)
+    {
             if (horizontal != 0 /* && !NewDialogueManager.GetInstance().dialogueIsPlaying */)
                ChangeAnimationState(RUN);
             else if (!isFallingAtMaxSpeed)
                 ChangeAnimationState(IDLE);
-    //}
-    //     else
-    //     {
-    //         if (IsSliding && !isSleepAnim)
-    //         {
-    //             ChangeAnimationState(SLIDING);
-    //         }
-    //         else if (!IsJumping && !isJumpFalling && !IsWallJumping && !NewPlayerCombat.instance.isAttacking && !NewPlayerCombat.instance.attackUp && !NewPlayerCombat.instance.attackDown && !isSleepAnim && !isAwakening && SceneManager.GetActiveScene().name != "T12Tower")
-    //         {
-    //             ChangeAnimationState(JUMP_FALLING);
-    //         }
-    //     }
+    }
+    else
+    {
+        if (IsSliding /*&& !isSleepAnim*/)
+        {
+            ChangeAnimationState(SLIDING);
+        }
+        else if (!IsJumping && !isJumpFalling && !IsWallJumping /*&& !NewPlayerCombat.instance.isAttacking && !NewPlayerCombat.instance.attackUp && !NewPlayerCombat.instance.attackDown && !isSleepAnim && !isAwakening && SceneManager.GetActiveScene().name != "T12Tower"*/)
+        {
+            ChangeAnimationState(JUMP_FALLING);
+        }
+    }
     }
     public void MoveInput(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+    }
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (isDashing) return;
+        //if (isSit || NewPlayerCombat.instance.isAttacking || NewPlayerCombat.instance.attackUp || NewPlayerCombat.instance.attackDown || NewDialogueManager.GetInstance().dialogueIsPlaying || isDoingSomething) return; 
+        if (context.performed /* && canJump*/)
+        {
+
+            OnJumpInput();
+
+            if (NewCanJump() && LastPressedJumpTime > 0)
+            {
+                // SALTO DELLA TORRE
+                // if (canPressOnlyJump)
+                // {
+                //     if(horizontal > 0)
+                //     {
+                //         IsJumping = true;
+                //         IsWallJumping = false;
+                //         isJumpCut = false;
+                //         isJumpFalling = false;
+
+                //         NewJump();
+
+                //         //StartCoroutine(DrakeScript.instance.DrakeFinish());
+                //     }
+                // }
+                // else 
+                // {
+                    IsJumping = true;
+                    IsWallJumping = false;
+                    isJumpCut = false;
+                    isJumpFalling = false;
+
+                    NewJump();
+                //}
+            }
+            else if(NewCanWallJump() && LastPressedJumpTime > 0 && canWallJumping)
+            {
+                IsWallJumping = true;
+                IsJumping = false;
+                isJumpCut = false;
+                isJumpFalling = false;
+                wallJumpStartTime = Time.time;
+                lastWallJumpDir = (LastOnWallRightTime > 0) ? -1 : 1;
+                IsAlreadySliding = false;
+
+                NewWallJump(lastWallJumpDir);
+            }
+        }
+        if (context.canceled && IsJumping && !canPressOnlyJump)
+        {
+            OnJumpUpInput();
+        }
+        
+    }
+    public void DashInput(InputAction.CallbackContext context)
+    {
+        //if (isSit) return;
+        if (context.performed && canDash && haveDash /*&& !NewDialogueManager.instance.dialogueIsPlaying*/)
+        {   
+            //if(PauseManager.paused) return;
+            if (!IsSliding) 
+            { 
+                StartCoroutine(NormalDash());
+                //dust.Play();
+            }
+            else if (IsSliding)
+            {
+                Flip();
+                StartCoroutine(DashWallSlide());
+            }
+        }
     }
 
     private void Inputs()
@@ -307,8 +394,8 @@ public class PlayerMovements : MonoBehaviour
 
     private void RunControlledJump()
     {
-        if (LastOnGroundTime < 0 && horizontal > 0)
-            Movement();
+        // if (LastOnGroundTime < 0 && horizontal > 0)
+        //     Movement();
     }
 
     private void CheckWorld()
@@ -316,6 +403,16 @@ public class PlayerMovements : MonoBehaviour
         //isTouchingWall = Physics2D.OverlapBox(wallCheckPoint.position, wallCheckSize, 0, wallslideLayer);
         inBorder = Physics2D.OverlapBox(inBorderStart.position, inBorderSize, 0, groundLayer);
     }
+
+    #region Timers Methods
+    private void Timers()
+    {
+        LastOnGroundTime -= Time.deltaTime;
+        LastOnWallTime -= Time.deltaTime;
+        LastOnWallRightTime -= Time.deltaTime;
+        LastOnWallLeftTime -= Time.deltaTime;
+    }
+    #endregion
 
     private void Movement()
     {
@@ -332,6 +429,7 @@ public class PlayerMovements : MonoBehaviour
         rb.linearVelocity = new Vector2(horizontal * move * moveSpeedIncrease, rb.linearVelocity.y);
     }
 
+    #region Flip
     public void DisableFlip()
     {
         canFlip = false;
@@ -357,6 +455,7 @@ public class PlayerMovements : MonoBehaviour
     }
     public void ManualFlip()
     {
+        // Flip the player via code
         if (facingRight || !facingRight)
         {
             facingRight = !facingRight;
@@ -369,6 +468,7 @@ public class PlayerMovements : MonoBehaviour
             // }
         }
     }
+    #endregion
     public void ChangeAnimationState(string newState)
     {
         //stop the same animation from interrupting itself
@@ -385,7 +485,7 @@ public class PlayerMovements : MonoBehaviour
     private void Gravity()
     {
         //Called in Update Method
-        // if (isDashing) return;
+        if (isDashing) return;
 
         // if (IsInTransition)
         // {
@@ -506,7 +606,269 @@ public class PlayerMovements : MonoBehaviour
         //rb.velocity = new Vector2(horizontal * move * moveSpeedIncrease, rb.velocity.y);
     }
     #endregion
+    #region DashFunctions
+    private IEnumerator NormalDash()
+    {
+        // NewPlayerCombat.instance.isAttacking = false;
+        // NewPlayerCombat.instance.attackDown = false;
+        // NewPlayerCombat.instance.attackUp = false;
+        canDash = false;
+        isDashing = true;
+        //PlayerLife.instance.isInvulnerable = true;
+        isDashingCooldown = true;
+        ChangeAnimationState(DASH);
+        transform.SetParent(null);
+        //CinemachineShake.Instance.ShakeCamera();
+        //FindObjectOfType<AudioManager>().Play("Dash");
+        //RumblePulse(0.1f, .12f, 0.25f);
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        // if(facingRight)
+        // {
+        //     Instantiate(dashParticleRight, transform.position, Quaternion.identity);
+        // }
+        // else if(!facingRight)
+        // {
+        //     Instantiate(dashParticleLeft, transform.position, Quaternion.identity);
+        // }
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        MaxFallSpeedCounter = 0;
+        if(LastOnGroundTime < 0)
+        {
+            ChangeAnimationState(JUMP_FALLING);
+        }
+        //PlayerLife.instance.isInvulnerable = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        isDashingCooldown = false;
+    }
+    private IEnumerator DashWallSlide()
+    {
+        canDash = false;
+        isDashing = true;
+        //PlayerLife.instance.isInvulnerable = true;
+        isDashingCooldown = true;
+        anim.SetBool("DoubleJump", false);
+        transform.SetParent(null);
+        anim.SetBool("isDashing", true);
+        //CinemachineShake.Instance.ShakeCamera();
+        //FindObjectOfType<AudioManager>().Play("Dash");
+        //RumblePulse(0.1f, .12f, 0.25f);
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(-transform.localScale.x * dashingPower, 0f);
+        isWallSliding = false;
+        anim.SetBool("isWallSliding", false);
+        facingRight = !facingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+        // if (facingRight)
+        // {
+        //     Instantiate(dashParticleRight, transform.position, Quaternion.identity);
+        // }
+        // else if (!facingRight)
+        // {
+        //     Instantiate(dashParticleLeft, transform.position, Quaternion.identity);
+        // }
+        yield return new WaitForSeconds(dashingTime);
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        //PlayerLife.instance.isInvulnerable = false;
+        anim.SetBool("isDashing", false);
+        yield return new WaitForSeconds(dashingCooldown);
+        isDashingCooldown = false;
+    }
+    private void ResetDash()
+    {
+        if(LastOnGroundTime > 0 && !IsSliding && !isDashingCooldown)
+        {
+            canDash = true;
+        }
+    }
+    // public void Stun()
+    // {
+    //     if(haveStunDash)
+    //     {
+    //         Collider2D[] detectedObjects = Physics2D.OverlapCircleAll(dashPos.position, dashStunRadius, enemyLayer);
+    //         foreach (Collider2D collider in detectedObjects)
+    //         {
+    //             collider.transform.parent.SendMessage("Stun");
+    //             Debug.Log("Stun by dash!");
+    //         }
+    //     }
+    // }
+    #endregion
+    #region Slide
+    private void SlideChecks()
+    {
+        if (CanSlide() && ((LastOnWallLeftTime > 0 && horizontal < 0) || (LastOnWallRightTime > 0 && horizontal > 0)))
+        {
+            IsSliding = true;
+        }
+        else
+        {
+            IsSliding = false;
+            /*
+            if (preventForceSlide)
+            {
+                preventForceSlide = false;
+            }
+            */
+        }              
+    }
+    public bool CanSlide()
+    {
+        if (LastOnWallTime > 0 && !IsJumping && !IsWallJumping && LastOnGroundTime <= 0 && canSliding)
+            return true;
+        else
+            return false;
+    }
+    private void Slide()
+    {
+        /*
+        if (!preventForceSlide)
+        {
+            rb.velocity = Vector2.zero;
+            preventForceSlide = true;
+        }
+        */
+        float speedDif = Data.slideSpeed - rb.linearVelocity.y;
+        float movement = speedDif * Data.slideAccel;
 
+        movement = Mathf.Clamp(movement, -Mathf.Abs(speedDif) * (1 / Time.fixedDeltaTime), Mathf.Abs(speedDif) * (1 / Time.fixedDeltaTime));
+
+        rb.AddForce(movement * Vector2.up);
+        IsAlreadySliding = true;
+    }
+    #endregion
+    #region Jump
+    private void NewJump()
+    {
+        //Make sure we can't call NewJump multiple times from one press
+        rb.linearVelocity = Vector2.zero;
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+        SetGravityScale(Data.gravityScale);
+        //dust.Play();
+
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        if (horizontal == 0)
+            ChangeAnimationState(JUMP);
+        else
+        {
+            if (canPressOnlyJump)
+                ChangeAnimationState("JumpBridgeUp");
+            else
+                ChangeAnimationState(JUMP_MOVING);
+        }
+    }
+    private void JumpChecks()
+    {
+        if(IsJumping && rb.linearVelocity.y < 0)
+        {
+            IsJumping = false;
+
+            if (!IsWallJumping)
+            {
+                isJumpFalling = true;
+                if (horizontal == 0 && !canPressOnlyJump /*&& !NewPlayerCombat.instance.isAttacking && !NewPlayerCombat.instance.attackUp && !NewPlayerCombat.instance.attackDown && SceneManager.GetActiveScene().name != "T12Tower" */)
+                {
+                    ChangeAnimationState(JUMP_FALLING);
+                }
+                else
+                    ChangeAnimationState(JUMP_MOVINGDOWN);
+                // else if (!NewPlayerCombat.instance.isAttacking && !NewPlayerCombat.instance.attackUp && !NewPlayerCombat.instance.attackDown && SceneManager.GetActiveScene().name != "T12Tower")
+                // {
+                //     if(canPressOnlyJump)
+                //         ChangeAnimationState("JumpBridgeDown");
+                //     else
+                //         ChangeAnimationState(JUMP_MOVINGDOWN);
+                // }
+            }
+        }
+        
+        if(IsWallJumping && Time.time - wallJumpStartTime > Data.wallJumpTime)
+        {
+            IsWallJumping = false;
+        }
+
+        if(LastOnGroundTime > 0 && !IsJumping && !IsWallJumping)
+        {
+            isJumpCut = false;
+
+            if (!IsJumping)
+                isJumpFalling = false;
+        }
+    }
+
+    private bool NewCanJump()
+    {
+        return LastOnGroundTime > 0 && !IsJumping;
+    }
+    public void OnJumpInput()
+    {
+        LastPressedJumpTime = Data.jumpInputBufferTime;
+    }
+    public void OnJumpUpInput()
+    {
+        if (CanJumpCut() || CanWallJumpCut())
+            isJumpCut = true;
+    }
+
+    #endregion
+    #region WallJump
+    private bool CanJumpCut()
+    {
+        return IsJumping && rb.linearVelocity.y > 0;
+    }
+    private bool CanWallJumpCut()
+    {
+        return IsWallJumping && rb.linearVelocity.y > 0;
+    }
+    private bool NewCanWallJump()
+    {
+        return LastPressedJumpTime > 0 && LastOnWallTime > 0 && LastOnGroundTime <= 0 && (!IsWallJumping || (LastOnWallRightTime > 0 && lastWallJumpDir == 1) /* || (LastOnWallLeftTime > 0 && lastWallJumpDir == -1) */);
+    }
+    private void NewWallJump(int dir)
+    {
+        isFallingAtMaxSpeed = false;
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+        LastOnWallRightTime = 0;
+        LastOnWallLeftTime = 0;
+        MaxFallSpeedCounter = 0;
+        //anim.SetTrigger("WallJump");
+
+        canFlip = false;
+        rb.linearVelocity = Vector2.zero;
+        Vector2 force = new Vector2(Data.wallJumpForce.x, Data.wallJumpForce.y);
+        force.x *= dir; //apply force in opposite direction of wall
+
+        
+        if (Mathf.Sign(rb.linearVelocity.x) != Mathf.Sign(force.x))
+            force.x -= rb.linearVelocity.x;
+
+        /*
+        if(rb.velocity.y < 0) //checks whether player is falling, if so we subtract the velocity.y (counteracting force of gravity). This ensures the player always reaches our deisre jump force or greater
+            force.y -= rb.velocity.y;
+        */
+        rb.AddForce(force, ForceMode2D.Impulse);
+        ManualFlip();
+        ChangeAnimationState(WALLJUMP);
+        StartCoroutine(WaitZeroForce());
+    }
+    private IEnumerator WaitZeroForce()
+    {
+        yield return new WaitForSeconds(.3f);
+        //rb.velocity = Vector2.zero;
+        canFlip = true;
+        //ChangeAnimationState(JUMP_FALLING);
+    }
+    #endregion
     #region OnDrawGizmos
     private void OnDrawGizmosSelected()
     {
